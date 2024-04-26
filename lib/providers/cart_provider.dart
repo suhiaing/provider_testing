52d6d869fbf118f0d800ddb23ff000f15d2dc07f
suhiaing <suhlaingthin288@gmail.com>
2024-04-26 17:36:10 +0630

    cart_provider done

--- lib/providers/cart_provider.dart
+++ lib/providers/cart_provider.dart
@@ -1,1 +1,32 @@
+import 'package:flutter/material.dart';
+import '../constants.dart';
 
+class CartProvider extends ChangeNotifier {
+  final List<Products> _items = [];
+
+  List<Products> get item => _items;
+
+  void nl() {
+    notifyListeners();
+  }
+
+  void addItem(Products item) {
+    _items.add(item);
+    nl();
+  }
+
+  void removeItem(Products item) {
+    _items.remove(item);
+    nl();
+  }
+
+  void deleteAllItem(List<Products> items) {
+    items.clear();
+    nl();
+  }
+
+  double getTotalPrice(List<Products> items) {
+    return items.fold(
+        0, (previousValue, element) => previousValue += element.price);
+  }
+}
